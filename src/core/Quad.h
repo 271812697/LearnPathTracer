efd35d99abb4380705c2f6316fc5d4bf89c3a677
271812697 <38773493+271812697@users.noreply.github.com>
2023-01-16 20:16:02 +0800

    init

---
+++ src/core/Quad.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Config.h"
+namespace GLSLPT
+{
+    class Program;
+
+    class Quad
+    {
+    public:
+        Quad();
+        void Draw(Program*);
+
+    private:
+        GLuint vao;
+        GLuint vbo;
+    };
+}
